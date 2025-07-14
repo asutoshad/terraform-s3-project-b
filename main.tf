784be2fcf4d29648f71caf224d2474c2d7677ef5
Aashutosh Adhikari <coolaashu55@gmail.com>
2025-07-14 11:45:44 +0545

    test 4

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "terraform_remote_state" "project_a" {
